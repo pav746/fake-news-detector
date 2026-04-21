5f0945a6986c041c62bb739933c951b15a3f648e
pav746 <pavneet04k@gmail.com>
2026-04-21 23:23:15 +0530

    fix: added deployed frontend URL to CORS

--- app.py
+++ app.py
@@ -14,7 +14,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://fake-news-detector-front.onrender.com"
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
